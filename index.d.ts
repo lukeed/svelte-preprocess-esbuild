5690459e89dcffc3530db76c28acc1afc22bddca
Luke Edwards <luke.edwards05@gmail.com>
2020-12-14 23:04:50 -0800

    fix(types): rework for strict key existence

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,28 @@
 import type { TransformOptions } from 'esbuild';
 import type { Preprocessor } from 'svelte/types/compiler/preprocess';
 
-export interface Options extends Omit<TransformOptions, 'logLevel'|'color'|'minify'|'jsxFactory'|'jsxFragment'|'color'|'logLevel'|'errorLimit'> {
-	/** @default 'tsconfig.json' */
-	tsconfig: string;
-	/** @default true */
-	sourcemap?: boolean | 'inline';
-	/** @default false */
-	minify?: boolean;
-	/** @default 'esm' */
-	format?: 'esm' | 'cjs';
-	/** @default 'utf8' */
-	charset?: TransformOptions['charset'];
-	/** @default 'error' */
-	loglevel?: TransformOptions['logLevel'];
+export type Definitions = {
+	[find: string]: string;
 }
 
+type Allow = Pick<TransformOptions, 'avoidTDZ'|'banner'|'charset'|'define'|'footer'|'keepNames'|'pure'|'target'|'treeShaking'|'tsconfigRaw'>;
+
+// export interface Options extends Omit<TransformOptions, 'logLevel'|'color'|'minify'|'jsxFactory'|'jsxFragment'|'color'|'logLevel'|'errorLimit'> {
+export interface Options extends Allow {
+	/** @default 'tsconfig.json' */
+	tsconfig?: string;
+	/** @default 'error' */
+	loglevel?: TransformOptions['logLevel'];
+	/** @default true */
+	sourcemap?: boolean | 'inline';
+	/** @default {} */
+	define?: Definitions;
+	/** @default 'utf8' */
+	charset?: TransformOptions['charset'];
+}
+
+/** @note Use `options.define` for replacements */
 export function typescript(options?: Options): { script: Preprocessor };
-export function replace(values: Record<string, string>): { script: Preprocessor };
+
+/** @note Only works with JavaScript! */
+export function replace(values: Definitions): { script: Preprocessor };
